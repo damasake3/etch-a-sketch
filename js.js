4cef8eb22371860a7fd389bced77b31283ca73a9
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-01 23:04:04 +0800

    squares are filled with color on mouseover

--- js.js
+++ js.js
@@ -2,10 +2,29 @@ const container = document.querySelector(".flex-container");
 const square = document.createElement("div");
 square.setAttribute("class", "flex-square");
 
-function generateSquares(){
+function generateSquares() {
     for (let i = 0; i < (16 * 16); i++) {
+        square.setAttribute("id", `square-${i + 1}`);
         container.appendChild(square.cloneNode(true));
     }
+
+    container.addEventListener("mouseover", (e) => {
+        let target = e.target;
+
+        if (target.className == "flex-square") {
+            console.log(`Hovering ${target.id} ${target.className}`);
+            let hoverSquare = document.getElementById(target.id);
+            
+            hoverSquare.setAttribute("class", "flex-square hovered");
+
+        }
+
+        
+    });
+
+
 }
+
+
 
 generateSquares();
