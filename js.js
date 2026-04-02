fa73ce2b3fcbca61dd6cdaf85942541bf0fa1cfc
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-02 14:52:36 +0800

    Added Range Validation

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".sketch-box");
 const square = document.createElement("div");
+const rangeInput = document.querySelector("#range");
+const rangeMsg = document.querySelector("#rangeMsg");
 square.setAttribute("class", "flex-square");
 
 function initializeSquares() {
@@ -8,15 +10,24 @@ function initializeSquares() {
 
 function generateSquares(s) {
     let size = s;
-    let squareSize = 100/size;
+    let squareSize = 100 / size;
     console.log(`size: ${s}`);
-    clearSquares();
 
-    for (let i = 0; i < (size * size); i++) {
-        square.setAttribute("id", `square-${i + 1}`);
-        square.setAttribute("style", `height: ${squareSize}%;  width: ${squareSize}%;`);
-        container.appendChild(square.cloneNode(true));
+    if (size < 1) {
+        rangeMsg.innerText = "Range [1-100] only";
+        rangeMsg.setAttribute("style", "color: red;");
     }
+    else {
+        clearSquares();
+        rangeMsg.innerText = "";
+        rangeMsg.removeAttribute("style", "color: red;");
+        for (let i = 0; i < (size * size); i++) {
+            square.setAttribute("id", `square-${i + 1}`);
+            square.setAttribute("style", `height: ${squareSize}%;  width: ${squareSize}%;`);
+            container.appendChild(square.cloneNode(true));
+        }
+    }
+
 }
 
 function clearSquares() {
@@ -24,8 +35,6 @@ function clearSquares() {
         container.removeChild(container.firstChild);
     }
 }
-
-
 
 container.addEventListener("mouseover", (e) => {
     let target = e.target;
