5356a2e83f8e6c65d2d93f965208bb5cde799d9c
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-02 14:15:04 +0800

    Added a range function to customize sketchbox size

--- js.js
+++ js.js
@@ -2,29 +2,21 @@ const container = document.querySelector(".sketch-box");
 const square = document.createElement("div");
 square.setAttribute("class", "flex-square");
 
-function generateSquares() {
-    let size = 54;
-    let squareSize = 100 / size;
+function initializeSquares() {
+    generateSquares(16);
+}
+
+function generateSquares(s) {
+    let size = s;
+    let squareSize = 100/size;
+    console.log(`size: ${s}`);
+    clearSquares();
 
     for (let i = 0; i < (size * size); i++) {
         square.setAttribute("id", `square-${i + 1}`);
         square.setAttribute("style", `height: ${squareSize}%;  width: ${squareSize}%;`);
         container.appendChild(square.cloneNode(true));
     }
-
-    container.addEventListener("mouseover", (e) => {
-        let target = e.target;
-
-        if (target.className == "flex-square") {
-            console.log(`Hovering ${target.id} ${target.className}`);
-            let hoverSquare = document.getElementById(target.id);
-            
-            hoverSquare.setAttribute("class", "flex-square hovered");
-
-        }
-
-    });
-
 }
 
 function clearSquares() {
@@ -33,4 +25,19 @@ function clearSquares() {
     }
 }
 
-generateSquares();
+
+
+container.addEventListener("mouseover", (e) => {
+    let target = e.target;
+
+    if (target.className == "flex-square") {
+        console.log(`Hovering ${target.id} ${target.className}`);
+        let hoverSquare = document.getElementById(target.id);
+
+        hoverSquare.setAttribute("class", "flex-square hovered");
+
+    }
+
+});
+
+initializeSquares();
